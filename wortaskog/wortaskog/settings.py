55a94ddb4b0226d232a90fde8baeceaf60862b13
Allan de Oliveira Almeida <arumeiraadan@outlook.com>
2025-03-10 00:19:33 -0300

    Updated SECRET_KEY

--- wortaskog/wortaskog/settings.py
+++ wortaskog/wortaskog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+from os import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n*b4p9bima38_h8ffw&47s08ckjqzkm=y$o(ie-cw$58n%u8($'
+SECRET_KEY = environ.get('WORTASKOG_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
